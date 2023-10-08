f29b8546aad5b1a6a24ca7d33b19d8ce84ae408d
mjDanny <bakhvalovfresh2014@gmail.com>
2023-10-08 23:06:29 +0300

    adding logout function

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def login():
     else:
         form = LoginForm()
         if form.validate_on_submit():
-            db_sess=db_session.create_session()
+            db_sess = db_session.create_session()
             user = db_sess.query(User).filter(User.email == form.username.data).first()
             if user and user.check_password(form.password.data):
                 login_user(user, remember=form.remember_me.data)
@@ -75,6 +75,13 @@ def login():
                             message='Неверный логин или пароль',
                             form=form)
         return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/')
 
 
 if __name__ == '__main__':
